e8f226b36bf29f789a6b6a7be9a9c1ea4fd43f16
zpackattack <Treyz3@icloud.com>
2020-04-16 21:27:10 -0400

    MOre

---
+++ GameJam2/Assets/MainMenu.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Audio;
+
+public class MainMenu : MonoBehaviour
+{
+    public AudioMixer audioMixer;
+    AudioListener AudioListener;
+    public AudioMixer SFXMixer;
+    public void MusicToggle()
+    {
+        AudioListener.pause = !AudioListener.pause;
+    }
+
+    public void SetVolume(float volume)
+    {
+        Debug.Log(volume);
+        audioMixer.SetFloat("MusicVol", volume);
+    }
+    public void SetSFXVolume(float volumeS)
+    {
+        Debug.Log(volumeS);
+        SFXMixer.SetFloat("SFXVol", volumeS);
+    }
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(3);
+    }
+    public void Home()
+    {
+        SceneManager.LoadScene(0);
+    }
+    public void Options()
+    {
+        SceneManager.LoadScene(2);
+    }
+    public void HowToPlay()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
